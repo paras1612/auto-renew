b04f0c00db45170943b25740500a913b6ca3bd14
paras1612 <44598977+paras1612@users.noreply.github.com>
2019-02-11 03:02:04 +0530

    Making the window invisible

--- renew.py
+++ renew.py
@@ -1,13 +1,17 @@
-# exclusively for IIIT Delhi :p
+# exclusively for IIIT Delhi students :p
 from selenium import webdriver
 username="username" #type your username
 password="password" #type your password
-driver=webdriver.Chrome()
+option = webdriver.ChromeOptions()
+option.add_argument("headless") #making chrome invisible XDDDD
+driver = webdriver.Chrome(options=option)
+driver.maximize_window()
+
 def login():
-    driver.get("http://library.iiitd.edu.in")
+    driver.get("http://library.iiitd.edu.in") 
     driver.find_element_by_id("userid").send_keys(username)
     driver.find_element_by_id("password").send_keys(password)
-    driver.find_element_by_css_selector(".action>.btn").click()
+    driver.execute_script("arguments[0].click();",driver.find_element_by_css_selector(".action>.btn"))
     driver.find_element_by_css_selector(".btn:nth-child(5)").click()
     driver.find_element_by_id("logout").click()
     driver.close()
